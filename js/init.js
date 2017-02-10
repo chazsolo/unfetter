4738d4522df56b7c9a9ec43a6341e416667ee9a0
Charles Grigg <cgrigg@adgcreative.net>
2017-02-10 11:50:48 -0500

    Refactor a lot of the structure of the site and fix some typos/grammar

--- js/init.js
+++ js/init.js
@@ -1,17 +1,24 @@
+(function iife( $ ) {
+    $(document).ready(function onReady() {
+        // init sidebar
+        $('.button-collapse').sideNav({
+            menuWidth: 320,
+            activationWidth: 70,
+            edge: 'left'
+        });
 
-
-$(document).ready(function(){
-      
-      // Sidebar
-      $(".button-collapse").sideNav({menuWidth: 320, activationWidth: 70, edge: 'left'});
-      // Dropdown
-      $('.dropdown-button').dropdown({
-           inDuration: 300,
-           outDuration: 225,
-           constrain_width: false, // Does not change width of dropdown to that of the activator
-           hover: false, // Activate on hover
-           gutter: 0, // Spacing from edge
-           belowOrigin: false // Displays dropdown below the button
-           }
-      );
+        // init dropdown
+        $('.dropdown-button').dropdown({
+            inDuration: 300,
+            outDuration: 225,
+            // does not change width of dropdown to that of the activator
+            constrain_width: false,
+            // activate on hover
+            hover: false,
+            // spacing from edge
+            gutter: 0,
+            // displays dropdown below the button
+            belowOrigin: false
+        });
     });
+})(jQuery);
